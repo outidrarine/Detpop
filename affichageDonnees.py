0f45d122802bfa918c221f4faacf821161882b95
Pierre <72155300+Pierre36@users.noreply.github.com>
2022-01-27 16:58:08 +0100

    Création de fonction pour l'affichage multicritère
    - Affichage de nuages de points pour les échantillonnages
    - Affichage du front de Pareto

--- affichageDonnees.py
+++ affichageDonnees.py
@@ -1,4 +1,4 @@
-# Librairies
+# LIBRAIRIES
 
 import sys
 import numpy as np
@@ -12,7 +12,7 @@ import csv
 from utils import getDateFromFilename, getPositionOfFilename, getAllDates, extract_birds, getPositionsOfFilenames
 from constructionPsi import compute_sample_pertinence, compute_average_similaritiy, get_all_pertinence, similarity_all, getpsi
 
-# Spectrogramme
+# SPECTROGRAMME
 
 def displaySpectrogram(sound, fs, title, ax):
     
@@ -24,7 +24,7 @@ def displaySpectrogram(sound, fs, title, ax):
     plot2d(spec_dB,**fig_kwargs, ax = ax, colorbar = False, now = False, cmap='viridis')
 
 
-# Representation temporelle
+# REPRESENTATION TEMPORELLE
 
 def displaySound(sound, fs, duration, title, ax):
 
@@ -37,7 +37,7 @@ def displaySound(sound, fs, duration, title, ax):
     ax.title.set_text(title)
 
 
-# Représentation polaire d'un échantillonnage de sons
+# REPRÉSENTATION POLAIRE D'UN ÉCHANTILLONNAGE DE SONS
 
 def displayPolarSamples(samples):
     nbSamples = len(samples)
@@ -78,7 +78,7 @@ def displayPolarSamples(samples):
     plt.show()
 
 
-# Tracé de la pertinence
+# TRACÉ DE LA PERTINENCE
 
 def displayPertinences(exagerated = False, samples = [], root = './SoundDatabase'):
 
@@ -112,7 +112,7 @@ def displayPertinences(exagerated = False, samples = [], root = './SoundDatabase
     scatter_over_pertinence(q, root, samples, exagerated)
 
 
-# Indique les échantillons selectionnés par un échantillonage dans la courbe des pertinences
+# INDIQUE LES ÉCHANTILLONS SELECTIONNÉS PAR UN ÉCHANTILLONAGE DANS LA COURBE DES PERTINENCES
 
 def scatter_over_pertinence(q, root, samples=[], exagerated = False):
 
@@ -124,7 +124,7 @@ def scatter_over_pertinence(q, root, samples=[], exagerated = False):
         plt.scatter(places, q['pertinence'][places], 50, marker='x', color = 'r')
 
 
-# Tracé de la matrice des similarités
+# TRACÉ DE LA MATRICE DES SIMILARITÉS
 
 def displaySimilarities(samples = [], root = './SoundDatabase'):
 
@@ -159,7 +159,7 @@ def displaySimilarities(samples = [], root = './SoundDatabase'):
     plt.colorbar(mappable = im, cax = cax)
 
 
-# Indique les échantillons selectionnés par un échantillonage dans la matrice des similarités
+# INDIQUE LES ÉCHANTILLONS SELECTIONNÉS PAR UN ÉCHANTILLONAGE DANS LA MATRICE DES SIMILARITÉS
 
 def scatter_over_similarity(root, samples=[]):
     
@@ -167,7 +167,7 @@ def scatter_over_similarity(root, samples=[]):
     plt.scatter(places, places, 200, marker='x', color = 'r')
 
 
-# Affichages d'informations sur un echantillonages
+# AFFICHAGES D'INFORMATIONS SUR UN ECHANTILLONAGES
 
 def present_sampling(sampling_function, nbSamples, exagerated_pertinences = False):
 
@@ -204,9 +204,26 @@ def present_sampling(sampling_function, nbSamples, exagerated_pertinences = Fals
     print(birds_set)
 
 
-# Comparaison d'échantillonages
+# COMPARAISON D'ÉCHANTILLONAGES
 
-def compare_sampling(sampling_list, sampling_names, nbSamples, nbSamplings, root = './SoundDatabase', persist_samples = False):
+def compare_sampling(sampling_list, sampling_names, nbSamples, nbSamplings, root = './SoundDatabase', clouds_alpha = 0.2, pareto = True):
+
+    # Calculs des échantillonages
+    average_pertinences, average_similarities, average_birds = compute_samplings(sampling_list, sampling_names, nbSamples, nbSamplings, root = root)
+
+    # Affichages des valeurs moyennes
+    displaySamplingsAverages(sampling_names, average_pertinences, average_similarities, average_birds)
+
+    # Affichages des histogrammes
+    displaySamplingsHistograms(sampling_names, average_pertinences, average_similarities)
+
+    # Affichage des nuages de points des échantillonnages
+    displaySamplingClouds(sampling_names, nbSamples, average_pertinences, average_similarities, clouds_alpha, pareto = pareto)
+
+
+# CALCUL DES ECHANTILLONNAGES POUR UNE LISTE D'ECHANTILLONNEURS
+
+def compute_samplings(sampling_list, sampling_names, nbSamples, nbSamplings, root = './SoundDatabase', persist_samples = False):
 
     # Initialisation
     average_pertinences = {}
@@ -221,10 +238,7 @@ def compare_sampling(sampling_list, sampling_names, nbSamples, nbSamplings, root
     # Calculs
     for s in range(nbSamplings):
 
-        # Affichage de la progression
-        sys.stdout.write('\r')
-        sys.stdout.write("[%-100s] %d%%" % ('='*round(s / (nbSamplings - 1) * 100), 100*s/(nbSamplings - 1)))
-        sys.stdout.flush()
+        progressbar(nbSamplings - 1, s)
 
         for k, sampling in enumerate(sampling_list):
             
@@ -237,18 +251,21 @@ def compare_sampling(sampling_list, sampling_names, nbSamples, nbSamplings, root
             # writing data to text file
             if(persist_samples):
                 exportSamplesToFiles(sampling_name, samples, s)
+    
+    return average_pertinences, average_similarities, average_birds
 
-    # Affichage des résultats
-    cols_names = ['Pertinences', 'Similarities']
-    rows_names = sampling_names
 
-    fig, axes = plt.subplots(len(sampling_names), 2, figsize=(12, 8))
+# AFFICHAGE D'UNE BARRE DE PROGRESSION
 
-    for ax, col in zip(axes[0], cols_names):
-        ax.set_title(col)
+def progressbar(max_progress, progress):
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-100s] %d%%" % ('='*round(progress / max_progress * 100), 100*progress/max_progress))
+    sys.stdout.flush()
 
-    for ax, row in zip(axes[:,0], rows_names):
-        ax.set_ylabel(row, rotation = 90, size='large')
+
+# AFFICHAGE DES VALEURS MOYENNES DES ECHANTILLONNAGES
+
+def displaySamplingsAverages(sampling_names, average_pertinences, average_similarities, average_birds):
 
     print()
 
@@ -262,12 +279,79 @@ def compare_sampling(sampling_list, sampling_names, nbSamples, nbSamplings, root
 
         print()
 
+
+# AFFICHAGES DES HISTOGRAMMES DES ECHANTILLONNAGES
+
+def displaySamplingsHistograms(sampling_names, average_pertinences, average_similarities):
+    
+    cols_names = ['Pertinences', 'Similarities']
+    rows_names = sampling_names
+
+    _, axes = plt.subplots(len(sampling_names), 2, figsize=(12, 8))
+
+    for ax, col in zip(axes[0], cols_names):
+        ax.set_title(col)
+
+    for ax, row in zip(axes[:,0], rows_names):
+        ax.set_ylabel(row, rotation = 90, size='large')
+
+    for k, sampling_name in enumerate(sampling_names):
+
         axes[k, 0].hist(average_pertinences[sampling_name], bins = 40, range = (0, 1))
 
         axes[k, 1].hist(average_similarities[sampling_name], bins = 40, range = (0, 1))
 
     plt.show()
 
+
+# REPRÉSENTATION DIVERSITÉ/PERTINENCE DES ÉCHANTILLONNAGES
+
+def displaySamplingClouds(sampling_names, nbSamples, average_pertinences, average_similarities, alpha, pareto = True):
+
+    plt.figure(figsize=(7, 7))
+
+    for k, sampling_name in enumerate(sampling_names):
+        plt.scatter(average_pertinences[sampling_name], 1 - average_similarities[sampling_name], alpha = alpha)
+
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+
+    plt.xlabel("Pertinence")
+    plt.ylabel("Diversité")
+    
+    plt.legend(sampling_names)
+
+    if pareto:
+
+        listX = np.block([average_pertinences[sampling_name] for sampling_name in sampling_names])
+        listY = 1 - np.block([average_similarities[sampling_name] for sampling_name in sampling_names])
+
+        paretoPointsX, paretoPointsY = findParetoPoints(listX, listY)
+
+        plt.plot(paretoPointsX, paretoPointsY, color = 'r')
+
+    plt.show()
+
+
+# RECHERCHE DES POINTS DU FRONT DE PARETO
+
+def findParetoPoints(listX, listY):
+
+    sortedPairs = sorted([[listX[i], listY[i]] for i in range(len(listX))], reverse = True)
+
+    paretoPoints = [sortedPairs[0]]    
+    
+    for pair in sortedPairs[1:]:
+        if pair[1] >= paretoPoints[-1][1]:
+            paretoPoints.append(pair)
+
+    paretoPointsX = [pair[0] for pair in paretoPoints]
+    paretoPointsY = [pair[1] for pair in paretoPoints]
+
+    return paretoPointsX, paretoPointsY
+
+
+# ENREGISTREMENT DES ÉCHANTILLONAGES DANS DES FICHIERS CSV
 
 def exportSamplesToFiles(sampling_name, samples, s):
     path = "./Results/" + sampling_name + "/"
