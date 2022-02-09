18688f3e093c6596a367e1be6298ecab0fb28c6f
Pierre <72155300+Pierre36@users.noreply.github.com>
2022-02-09 16:59:20 +0100

    Vraisemblance pour échantillonnage aléatoire et par pertinence

---
+++ likelihood.py
@@ -0,0 +1,26 @@
+# Imports
+
+import math
+import numpy as np
+
+from constructionPsi import getPertinences
+from utils import getPositionsOfFilenames
+
+
+# Vraisemblance pour echantillonage random
+
+def randomLikelihood(samples, nbSounds = 432):
+    return 1 / math.comb(nbSounds, len(samples))
+
+
+# Vraissemblance pour echantillonage par pertinence
+
+def pertinenceLikelihood(samples, pertinenceFunction = 'identity', root = './SoundDatabase'):
+
+    q = getPertinences(pertinenceFunction = pertinenceFunction, root = root, verbose = False)
+
+    positions = getPositionsOfFilenames(root, samples)
+
+    samplesPertinences = q[positions]
+    
+    return np.prod(samplesPertinences / np.sum(q))
